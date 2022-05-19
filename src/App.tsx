869bd2c6bccbd0d672ac0eefdb6102b24e912f69
Julien Pause <julien.pause@epitech.eu>
2022-05-19 19:14:14 +0200

    fix: compilation errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import moment from 'moment';
-import 'moment/locale/fr';
-
-import StackNavigation from './navigation/Stack'
+import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
-  moment.locale("fr")
-
   return (
-    <StackNavigation />
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>
+        This is a React Native snapshot test.
+      </Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  instructions: {
+    color: '#333333',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center',
+  },
+});
 
 export default App

9439eb577feb2d500702a5ca0ce63ab084f23e2e
Dorian.bse <dorian.beasse@epitech.eu>
2022-11-30 22:54:47 -0800

    update: mock/reports.tsx

--- src/mock/reports.tsx
+++ src/mock/reports.tsx
@@ -8,7 +8,7 @@ import Report from "../types";
 
 const mockReports: Report[] = [
 {
-    date: '2022-01-08',
+    date: '2022-11-08',
     symptoms: {
       period: {
           pain: 8,
@@ -41,85 +41,103 @@ const mockReports: Report[] = [
     }
 },
 {
-    date: '2022-01-10',
+    date: '2022-11-03',
+    symptoms: {
+      period: {
+          pain: 4,
+          flow: 2
+      },
+      digestiveDisorders: {
+          pain: 3,
+          presenceOfBlood: false,
+          diarrhea: true,
+          constipation: false
+      },
+      urinaryDisorders: {
+          pain: 1,
+          burn: false,
+          presenceOfBlood: false,
+          troubleEmptyingBladder: true,
+          frequentUrges: false
+      },
+      fatigue: {
+        fatigue: true,
+        lowerBackPain: true,
+        pelvicPain: false,
+      },
+      pain: {
+        painDuringSex: true,
+      },
+      medicines: {
+        medicines: [""]
+      }
+    }
+},
+{
+  date: '2022-11-15',
+  symptoms: {
     period: {
-        pain: 4,
-        flow: 2
+      pain: 0,
+      flow: 0
     },
     digestiveDisorders: {
-        pain: 3,
-        presenceOfBlood: false,
-        diarrhea: true,
-        constipation: false
+      pain: 6,
+      presenceOfBlood: true,
+      diarrhea: false,
+      constipation: true
     },
     urinaryDisorders: {
-        pain: 1,
-        burn: false,
-        presenceOfBlood: false,
-        troubleEmptyingBladder: true,
-        frequentUrges: false
+      pain: 6,
+      burn: false,
+      presenceOfBlood: true,
+      troubleEmptyingBladder: true,
+      frequentUrges: true
     },
     fatigue: {
       fatigue: true,
       lowerBackPain: true,
       pelvicPain: false,
     },
-    painDuringSex: false,
-    medicines: [""]
+    pain: {
+      painDuringSex: false,
+    },
+    medicines: {
+      medicines: [""]
+    }
+  }
 },
 {
-  date: '2022-01-13',
-  period: {
-    pain: 0,
-    flow: 0
-  },
-  digestiveDisorders: {
-    pain: 6,
-    presenceOfBlood: true,
-    diarrhea: false,
-    constipation: true
-  },
-  urinaryDisorders: {
-    pain: 6,
-    burn: false,
-    presenceOfBlood: true,
-    troubleEmptyingBladder: true,
-    frequentUrges: true
-  },
-  fatigue: {
-    fatigue: true,
-    lowerBackPain: true,
-    pelvicPain: false,
-  },
-  painDuringSex: false,
-  medicines: [""]
-},
-{
-  date: '2022-01-16',
-  period: {
-    pain: 10,
-    flow: 10
-  },
-  digestiveDisorders: {
-    pain: 10,
-    presenceOfBlood: true,
-    diarrhea: false,
-    constipation: true
-  },
-  urinaryDisorders: {
-    pain: 10,
-    burn: true,
-    presenceOfBlood: true,
-    troubleEmptyingBladder: true,
-    frequentUrges: true
-  },
-  fatigue: {
-    fatigue: true,
-    lowerBackPain: true,
-    pelvicPain: true
-  },
-  painDuringSex: true,
-  medicines: [""]
+  date: '2022-12-01',
+  symptoms: {
+    period: {
+      pain: 10,
+      flow: 10
+    },
+    digestiveDisorders: {
+      pain: 10,
+      presenceOfBlood: true,
+      diarrhea: false,
+      constipation: true
+    },
+    urinaryDisorders: {
+      pain: 10,
+      burn: true,
+      presenceOfBlood: true,
+      troubleEmptyingBladder: true,
+      frequentUrges: true
+    },
+    fatigue: {
+      fatigue: true,
+      lowerBackPain: true,
+      pelvicPain: true
+    },
+    pain: {
+      painDuringSex: false,
+    },
+    medicines: {
+      medicines: [""]
+    }
+  }
 }
 ]
 
